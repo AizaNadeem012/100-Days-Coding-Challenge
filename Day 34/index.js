2271548c1c4b3acb47dd04f2501c45e4ad3c7233
AizaNadeem0000 <aizanadem333@gmail.com>
2024-04-04 10:19:03 -0700

    add New File

---
+++ Day 34/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// Question No 1:
+Object.defineProperty(exports, "__esModule", { value: true });
+let square = Math.sqrt(144);
+console.log(square);
+// Question No 2:
+function integerNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(integerNumber());
+// Question No 3:
+let num = Math.abs(-5);
+console.log(num);
---
+++ Day 34/index.ts
@@ -0,0 +1,19 @@
+// Question No 1:
+
+let square: number = Math.sqrt(144);
+
+
+console.log(square);
+
+// Question No 2:
+
+function integerNumber(): number{
+    return Math.floor(Math.random() * 10 )+ 1
+}
+console.log(integerNumber());
+
+// Question No 3:
+
+let num: number = Math.abs(-5);
+
+console.log(num);
