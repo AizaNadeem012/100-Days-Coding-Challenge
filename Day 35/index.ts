cd56e96b515a37677a016ad228a536e7740c8f1a
Aiza Nadeem <164611470+AizaNadeem012@users.noreply.github.com>
2024-04-05 09:27:28 -0700

    Update index.ts index.js

--- Day 35/index.ts
+++ Day 35/index.ts
@@ -1,7 +1,4 @@
-// // Question No 1:
-
-// let a = 4;
-
+// Question No 1:
 
 function randomBoolean(): boolean{
     return Math.random() > 5;
@@ -9,12 +6,16 @@ function randomBoolean(): boolean{
 
 console.log(randomBoolean());
 
+// Question No 2:
+
 function generateColor(){
     let color = '#' + Math.floor(Math.random()* 0xFFFFFF).toString(16).padStart(6 , '0')
     return color;
 }
 
 console.log(generateColor());
+
+// Question No 3:
 
 
 function randomInt(): number{
