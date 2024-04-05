16edb8c2d59ab35d8341c45d9315923ad1bd6b8d
Aiza Nadeem <164611470+AizaNadeem012@users.noreply.github.com>
2024-04-05 09:28:28 -0700

    Update index.js

--- Day 35/index.js
+++ Day 35/index.js
@@ -1,15 +1,20 @@
 "use strict";
 // // Question No 1:
 Object.defineProperty(exports, "__esModule", { value: true });
-// let a = 4;
+
 function randomBoolean() {
     return Math.random() > 5;
 }
 console.log(randomBoolean());
+
+/Question No 2:
+
 function generateColor() {
     let color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
     return color;
 }
+// Question No 3:
+
 console.log(generateColor());
 function randomInt() {
     return Math.floor(Math.random() * 6) + 1;
