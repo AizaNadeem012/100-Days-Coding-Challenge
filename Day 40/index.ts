fc45ef4968976afd39daf79d22afdd1486ced4b0
AizaNadeem0000 <aizanadem333@gmail.com>
2024-04-14 14:12:01 -0700

    add New File

---
+++ Day 40/index.ts
@@ -0,0 +1,38 @@
+// Question No 1:
+
+for(let i = 0; i <= 10; i++ ){
+    console.log(i);
+    
+}
+
+// Question No 2:
+
+let count: number = 0
+while( count < 5){
+    console.log("Hello World");
+    count ++;
+    
+}
+
+// Question No 3:
+
+let movies: string[] = [
+    '12 Angry Men',
+    'Pulp Fiction',
+     'The Lord of the Rings',
+     'The Shawshank Redemption'
+]
+
+for(let movie of movies){
+    console.log(movie);
+    
+}
+
+
+
+
+
+
+
+
+
