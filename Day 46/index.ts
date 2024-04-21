03edcb815b7deab599c4447fa5a7b17bd2af37a9
AizaNadeem0000 <aizanadem333@gmail.com>
2024-04-21 04:52:03 -0700

    add New File

---
+++ Day 46/index.ts
@@ -0,0 +1,24 @@
+
+// Question No 1:
+
+for (let i = 1; i <= 5; i++) {
+    console.log(`Iteration ${i}, i value:, i`);
+  }
+  //Question 2
+  let a = 20;
+  let b = 10;
+  console.log(a);
+  try {
+    console.log(c);
+  } catch (error) {
+    console.log(error.message);
+  }
+  console.log(b);
+  //Question 3
+  // Sample code snippet for setting a breakpoint
+  console.log("Before breakpoint");
+  // Set a breakpoint on the following line using your browser's developer tools
+  console.log(
+    "This line has a breakpoint set on it in the browser's developer tools"
+  );
+  console.log("After breakpoint");
