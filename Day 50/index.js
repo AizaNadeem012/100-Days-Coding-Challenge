516dcd8629e2f81380a2012669f01a2f65e3152a
AizaNadeem0000 <aizanadem333@gmail.com>
2024-05-02 11:34:06 -0700

    add New File

---
+++ Day 50/index.js
@@ -0,0 +1,23 @@
+"use strict";
+// Question No 1:
+Object.defineProperty(exports, "__esModule", { value: true });
+setTimeout(() => {
+    console.log("Hello Aiza");
+}, 4000);
+// Question No 2:
+setTimeout(() => {
+    console.log("This is me aiza nadeem");
+}, 1000);
+setTimeout(() => {
+    console.log("This is me areeba nadeem");
+}, 2000);
+console.log("This is wajahat nadeem");
+//     // Question No 3:
+console.log("Synchronous Function");
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log("Asynchronous Function");
+setTimeout(() => {
+    console.log("This is asynchronous function");
+}, 2000);
---
+++ Day 56/index.ts
@@ -0,0 +1,60 @@
+
+// STRING CONVERSION:
+
+let value = true;
+
+console.log((typeof value)
+);
+
+// Return Boolean :
+
+let val = "Aiza"
+console.log(typeof val);
+
+// Return string:
+
+// NUMERIC CONVERSION:
+
+let str1 = "345"
+
+console.log(typeof str1); //Return String
+
+let str2 = 234
+
+console.log(typeof str2); //Return Number
+
+// BOOLEAN CONVERSION:
+
+console.log( Boolean(3));
+console.log(Boolean(0));
+
+console.log(Boolean("Hello"));
+console.log(Boolean(""));
+
+
+//-----------------------------***********************-------------------------------//
+
+// URINARY OPERATOR:
+
+let a = 3;
+
+a = -a;
+console.log(a);
+
+
+// BINARY OPERATOR:
+
+let p = 100, q = 80;
+alert( p - q ); 
+
+
+
+
+
+
+
+
+
+
+
+
