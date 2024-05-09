4c8f68e1a95accdb915c80b39b5923e5491e3266
AizaNadeem0000 <aizanadem333@gmail.com>
2024-05-09 07:38:02 -0700

    add New File

--- Day 58/index.ts
+++ Day 58/index.ts
@@ -1,46 +1,46 @@
 
-// Logical operators
+// // Logical operators
 
-// OR Operator:
+// // OR Operator:
 
-let a = 7;
-let b = 8;
+// let a = 7;
+// let b = 8;
 
-console.log(a < b || a > b );
+// console.log(a < b || a > b );
 
 
-// AND Operator:
+// // AND Operator:
 
-let c = 9;
+// let c = 9;
 
-let d = 8;
+// let d = 8;
 
-console.log(c != 9 && d == 8);
+// console.log(c != 9 && d == 8);
 
-// NOT Operator:
+// // NOT Operator:
 
-let e = !true;
+// let e = !true;
 
-let f = !false;
+// let f = !false;
 
-console.log(e, f);
+// console.log(e, f);
 
 
 
-//Nullish coalescing operator '??' Check Undefined and Null
+// //Nullish coalescing operator '??' Check Undefined and Null
 
 
-let user = {
-    student: {
-        name: null,
-        age: 0,
+// let user = {
+//     student: {
+//         name: null,
+//         age: 0,
 
 
-    }
-}
+//     }
+// }
 
-console.warn(user.student.name ?? "Unknown");
-console.warn(user.student.age ?? 18 );
+// console.warn(user.student.name ?? "Unknown");
+// console.warn(user.student.age ?? 18 );
 
 
 
@@ -91,6 +91,11 @@ console.warn(user.student.age ?? 18 );
 
 
 
+
+
+let x : unknown = "aiza";
+
+console.log(typeof x);
 
 
 
---
+++ Day 60/README.md
@@ -0,0 +1,12 @@
+
+Day-60 of 100-Days-Of-Code Challenge
+Day-60 Task:
+
+Read the following Articles and Code along with it:
+
+
+https://javascript.info/function-basics
+
+
+
+https://javascript.info/function-expressions
